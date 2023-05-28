c2c4d76940e1571a2953f95e44023e7efc396b6f
john clampet <jsclampet@gmail.com>
2023-05-28 17:31:05 -0500

    added NavBar and content with property binding and even binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pagesFromScratch';
+  currentPage: number = 0;
+  images = [
+    {
+      title: 'At the beach',
+      url: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhY2h8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: 'In the mountains',
+      url: 'https://images.unsplash.com/photo-1454496522488-7a8e488e8606?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bW91bnRhaW58ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: 'In the desert',
+      url: 'https://images.unsplash.com/photo-1542401886-65d6c61db217?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZGVzZXJ0fGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: 'In the City',
+      url: 'https://plus.unsplash.com/premium_photo-1675198764206-40536cd9c362?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bnljfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: 'Riding a bike',
+      url: 'https://images.unsplash.com/photo-1597840972104-54eb08032a44?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cmlkaW5nJTIwYSUyMGJpa2V8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: 'At the show',
+      url: 'https://images.unsplash.com/photo-1459749411175-04bf5292ceea?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Y29uY2VydHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60'
+    }
+  ]
 }
